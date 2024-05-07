2a4fee1008c6d30d45cc6c2b6b33ca9801992d52
deena <deenaantonym@gmail.com>
2024-05-07 18:01:47 +0530

    Initial commit

---
+++ src/test/java/com/example/commserviceapr24mor/Commserviceapr24morApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commserviceapr24mor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Commserviceapr24morApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
